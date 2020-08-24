b4c02e84fce0b94516e511049121fc41cc2b6f64
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-24 01:11:08 -0700

    created fetch request for user login

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 const Header = (props) => {
     return (
         <header>
-            <div classname="links">
+            <div className="links">
                 <ul>
                     { props.currentUser ?
                         <>
                             <li><Link to={ '/profile' }>Profile</Link></li>
-                            <li><a href='/logout' onClick={ props.logout }>Log Out</a></li>
+                            <li><a href="/logout" onClick={ props.logout }>Log Out</a></li>
                         </>
                     :
                         <>
-                            <li><Link to={'/register'}>Register</Link></li>
-                            <li><Link to={'/login'}>Login</Link></li>                
+                            <li><Link to={ '/register' }>Register</Link></li>
+                            <li><Link to={ '/login' }>Login</Link></li>                
                         </>
                     }
                 </ul>
--- src/models/user.js
+++ src/models/user.js
@@ -9,4 +9,14 @@ export default class UserModel {
         })
         return await response.json()
     }
+    static async login(credentials) {
+        // remember to add "credentials: include"
+        const response = await fetch(`${REACT_APP_API_URL}/auth/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify(credentials)
+         })
+         return await response.json()
+     }
 }
