dfb4ead9092240b98a10cd9a878ce25f2b82de98
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-29 09:49:20 -0700

    added heroku api url for deploy

--- src/models/drink.js
+++ src/models/drink.js
@@ -1,4 +1,4 @@
-const url = 'process.env.REACT_APP_API_URL || http://localhost:4000/api/v1'
+const url = process.env.REACT_APP_API_URL || 'http://localhost:4000/api/v1'
 
 class DrinkModel {
     static all = async () => {
